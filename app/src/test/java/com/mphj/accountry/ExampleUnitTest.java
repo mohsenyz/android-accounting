8e4262d08569ba4d283511be26711ee39fd940b2
mphj <alimohammadi340@gmail.com>
2017-11-16 16:10:19 -0500

    Test stage removed from ci

--- app/src/test/java/com/mphj/accountry/ExampleUnitTest.java
+++ app/src/test/java/com/mphj/accountry/ExampleUnitTest.java
@@ -11,7 +11,10 @@ import static org.junit.Assert.assertTrue;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
