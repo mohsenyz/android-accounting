302d0be0fbd4969893e6c8090f6abe0e4225c53a
mphj <alimohammadi340@gmail.com>
2017-12-14 21:23:13 +0330

    Added splash screen

--- app/src/main/java/com/mphj/accountry/activity/MainActivity.java
+++ app/src/main/java/com/mphj/accountry/activity/MainActivity.java
@@ -3,14 +3,11 @@ package com.mphj.accountry.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.mphj.accountry.R;
-
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         startActivity(new Intent(this, LoginActivity.class));
         finish();
     }
